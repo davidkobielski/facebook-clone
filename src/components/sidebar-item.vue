8e9cfd47bf8849661d903c33d3b6bfef5d630bc1
Extr4v3rT <dkobiels@gmail.com>
2021-10-13 03:53:22 +0200

    First concept of home sidebar

---
+++ src/components/sidebar-item.vue
@@ -0,0 +1,24 @@
+<template>
+
+<div class="w-4/5 h-12 pl-2 hover:bg-gray-500 hover:bg-opacity-30 rounded-lg">
+  <router-link to="#" class="flex items-center w-full h-full">
+      <img :src="icon" :alt="name" class="w-9 h-9 rounded-full">
+      
+    <div class=" bg-gray-600"></div>
+    <span class="font-bold ml-2 text-gray-200 text-base">{{name}}</span>
+  </router-link>
+  </div>
+</template>
+
+<script>
+export default {
+    props: {
+        icon: String,
+        name: String,
+    },
+  name: 'SidebarItem',
+}
+</script>
+
+<style>
+</style>
