79a0415ba50a77acdafbc01be4afb65c5b89a423
Extr4v3rT <dkobiels@gmail.com>
2021-10-13 13:04:22 +0200

    created more advanced concept of sidebar

--- src/components/sidebar-item.vue
+++ src/components/sidebar-item.vue
@@ -1,20 +1,30 @@
 <template>
-
-<div class="w-4/5 h-12 pl-2 hover:bg-gray-500 hover:bg-opacity-30 rounded-lg">
-  <router-link to="#" class="flex items-center w-full h-full">
-      <img :src="icon" :alt="name" class="w-9 h-9 rounded-full">
+<div class="w-4/5 h-14 py-1 pl-2 hover:bg-gray-500 hover:bg-opacity-30 rounded-lg text-left">
+  <router-link :to="to" class="flex items-center w-full h-full">
+      <img :src="icon" :alt="name" class="img w-10 h-10 rounded-full">
       
     <div class=" bg-gray-600"></div>
-    <span class="font-bold ml-2 text-gray-200 text-base">{{name}}</span>
+    <span class="font-semibold ml-2 text-gray-300 text-base">{{name}}</span>
   </router-link>
   </div>
 </template>
 
 <script>
 export default {
+  // mounted() {
+  //   const img = document.querySelector('.img');
+  //   console.log(document.querySelector('.img').naturalWidth);
+  //   console.log(document.querySelector('.img').naturalHeight);
+  //   if(img.naturalWidth == img.naturalHeight) {
+  //     img.classList.add('rounded-full')
+  //   } else {
+  //     img.classList.add('rounded-xl')
+  //   }
+  // },
     props: {
         icon: String,
         name: String,
+        to: String,
     },
   name: 'SidebarItem',
 }
